363003c1cdda626729b4fd1307202bd42019aa73
AnhHN <anhhn@vnext.vn>
2023-12-21 00:47:05 +0700

    di chuyển và camera di chuyển theo nhân vật

--- Assets/Scripts/DiChuyen.cs
+++ Assets/Scripts/DiChuyen.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DiChuyen : MonoBehaviour
 {
     public Animator animator;
     public bool isRight = true;
+    private Rigidbody2D rb;
+    private bool nen;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,19 +21,52 @@ public class DiChuyen : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            isRight = true;
             animator.SetBool("isRunning", true);
             transform.Translate(Time.deltaTime * 5, 0, 0);
             Vector2 scale = transform.localScale;
             scale.x *= scale.x > 0 ? 1 : -1;
             transform.localScale = scale;
-        }    
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            isRight = false;
             animator.SetBool("isRunning", true);
-            transform.Translate( - Time.deltaTime * 5, 0, 0);
+            transform.Translate(-Time.deltaTime * 5, 0, 0);
             Vector2 scale = transform.localScale;
             scale.x *= scale.x > 0 ? -1 : 1;
             transform.localScale = scale;
-        }    
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (nen == true)
+            { 
+                if (isRight == true)
+                {
+                    // transform.Translate(Time.deltaTime * 5, Time.deltaTime * 10, 0);
+                    rb.AddForce(new Vector2(0, 500));
+                    Vector2 scale = transform.localScale;
+                    scale.x *= scale.x > 0 ? 1 : -1;
+                    transform.localScale = scale;
+                }
+                else
+                {
+                    // transform.Translate(-Time.deltaTime * 5, Time.deltaTime * 10, 0);
+                    rb.AddForce(new Vector2(0, 500));
+                    Vector2 scale = transform.localScale;
+                    scale.x *= scale.x > 0 ? -1 : 1;
+                    transform.localScale = scale;
+                }
+                nen = false;
+            }
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "nen_dat")
+        {
+            nen = true;
+        }
+    }
+
 }
